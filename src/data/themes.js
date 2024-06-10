bd981c3b816d27e2833634e372761fc25dbb26d8
SAYEERA <sayeera56@gmail.com>
2024-06-10 08:57:57 -0500

    second Commit

--- src/data/themes.js
+++ src/data/themes.js
@@ -1,5 +1,5 @@
 import { createTheme } from "@mui/material";
-import resume from "./Sayeera Resume.pdf"
+import resume from "./Sayeera's Resume.pdf"
 
 export const darkTheme = createTheme({palette: {mode: "light"}})
 export  {resume}
